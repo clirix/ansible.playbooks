53b09466bddfc634040025b7b5e6fca35555b318
sven <sven@so36.net>
2013-08-27 13:03:09 +0200

    Change poweroff to acpipowerbutton

--- scripts/vbox_stop.sh
+++ scripts/vbox_stop.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
-VBoxManage controlvm ubuntu-plonegov-cloud20august poweroff
+#VBoxManage controlvm ubuntu-plonegov-cloud20august poweroff
+VBoxManage controlvm ubuntu-plonegov-cloud20august acpipowerbutton
